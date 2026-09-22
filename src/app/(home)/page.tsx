00b1d17fdf979a2d162416a4feb695819ad6d00f
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-22 11:30:29 -0300

    Implementei como a listagem funcionará em diferentes tamanhos de tela usando display grid

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,13 +2,90 @@ import "./home.css"
 
 export default function Home() {
   return (
-    <section className="container">
-      <h1>Home!</h1>
+    <section className="home container">
 
       <div className="produtos">
+
         <div className="produto">
-          
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
         </div>
+
+        <div className="produto">
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
+        </div>
+
+        <div className="produto">
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
+        </div>
+
+        <div className="produto">
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
+        </div>
+
+        <div className="produto">
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
+        </div>
+
+        <div className="produto">
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
+        </div>
+
+        <div className="produto">
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
+        </div>
+
+        <div className="produto">
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
+        </div>
+
+        <div className="produto">
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
+        </div>
+
+        <div className="produto">
+          <img 
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
+          />
+          <h3>Fone de ouvido</h3>
+          <span>R$ 230,00</span>
+        </div>
+
       </div>
 
     </section>
