426261e2c7e1ec67fcbe1d02fa485c28c4f585cf
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-24 09:44:19 -0300

    A listagem já está pegando os produtos da API de forma dinâmica e renderizando na tela

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,21 +8,37 @@ import api from "@/config/api";
 export default function Home() {
   const [produtos, setProdutos] = useState<ProdutoInterface[]>()
 
+  useEffect(()=> {
+    async function getProdutos() {
+      try {
+        const response = await api.get("/products")
+        setProdutos(response.data)
+      } catch(erro) {
+        console.log(erro)
+      }
+    }
+    getProdutos()
+  }, [])
+
   return (
     <section className="home container">
 
       <div className="produtos">
 
-        <div className="produto">
-          <img 
-            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuMEbQMWrh--PaEr1HGx4kVJudIUOTudNp0Kh1AwjmaN0HizkVQgIouKO&s=10`} 
-          />
-          <h3>Fone de ouvido</h3>
-          <div className="infos">
-            <span>R$ 230,00</span>
-            <i className="fa-solid fa-cart-plus fa-lg"></i>
-          </div>
-        </div>
+        { produtos?.map((produto)=> {
+          return (
+            <div key={produto.id} className="produto">
+              <img 
+                src={produto.cover} 
+              />
+              <h3>{produto.title.split(" ").slice(0, 3).join(" ")}...</h3>
+              <div className="infos">
+                <span>R$ {produto.price}</span>
+                <i className="fa-solid fa-cart-plus fa-lg"></i>
+              </div>
+            </div>
+          )
+        }) }
 
       </div>
 
